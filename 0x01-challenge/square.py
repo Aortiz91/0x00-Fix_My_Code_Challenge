862d95ee452b7a7e5b4057b7363a49d997a96ca1
Andy <3283@holbertonschool.com>
2022-02-20 20:40:56 -0300

    Added doc

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,7 +10,7 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Documentation """
+        """ Constructor """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -23,10 +23,10 @@ class square():
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Represents object as string """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
